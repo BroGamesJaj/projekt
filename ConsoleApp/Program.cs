ab0071a5b4774e5b6a0cf0b4b0e14b1329a03a81
BroGames <hunempire11@gmail.com>
2023-12-18 12:34:59 +0100

    3.feladat lista elemei kiiratása

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,9 +1,12 @@
 ﻿List<int> lista = new List<int>();
 int nszam = 1;
 while(nszam != 0){
+    Console.WriteLine(string.Join(",", lista));
     Console.WriteLine("Kérem adjon meg egy számot a listához.(0 érték a végjel)");
     nszam = int.Parse(Console.ReadLine());
     if(nszam != 0){
         lista.Add(nszam);
     }
 }
+
+Console.WriteLine(string.Join(",", lista));
