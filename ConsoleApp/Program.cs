48a6b3d11aee4852d69cfd41a1059224c64e481f
BroGames <hunempire11@gmail.com>
2023-12-18 12:23:35 +0100

    1.feladat szám bekérése a felhasználótól a listába, 0 végjelig

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿List<int> lista = new List<int>();
+int nszam = 1;
+while(nszam != 0){
+    Console.WriteLine("Kérem adjon meg egy számot a listához.(0 érték a végjel)");
+    nszam = int.Parse(Console.ReadLine());
+    if(nszam != 0){
+        lista.Add(nszam);
+    }
+}
